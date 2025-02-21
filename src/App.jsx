693ab61e495447f477bc4f2f804663a27f09e035
Rahul Kumar <124748609+Rahwulkumar@users.noreply.github.com>
2025-02-21 13:15:09 +0530

    Made changes to the login and working on signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import './styles/App.css';
-import Footer from './components/Footer/Footer';
-import About from './components/About/About';
-import Team from './components/Team/Team';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Team from "./components/Team/Team";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import "./styles/App.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Team />
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Hero />
-      <About/>
-      <Team/>
-      <Footer/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/signup" element={<Signup />} /> */}
+    </Routes>
   );
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,39 +1,23 @@
-// import React from "react";
-// import "./Login.css";
+import React from "react";
+import "./Login.css";
+import logo from "../../assets/icons/logo.svg"
 
-// const Login = () => {
-//   return (
-//     <div className="login-container">
-//       <div className="login-box">
-//         <div className="login-form">
-//           <h1>Health Inspect&r</h1>
-//           <h2>Login</h2>
-//           <p>Welcome back! Please login to your account.</p>
-          
-//           <label htmlFor="username">User Name</label>
-//           <input type="text" id="username" placeholder="Justnibieber@gmail.com" />
-          
-//           <label htmlFor="password">Password</label>
-//           <input type="password" id="password" placeholder="********" />
-          
-//           <div className="login-options">
-//             <div>
-//               <input type="checkbox" id="remember" />
-//               <label htmlFor="remember">Remember me</label>
-//             </div>
-//             <a href="#">Forgot Password?</a>
-//           </div>
+const Login = () => {
+  return (
+    
+    <div className="login-container">
+        <div className="navbar-logo">
+                <img src={logo} alt="Health Inspector" />
+                <span>Health Inspector</span>
+              </div>
+      <div className="login-box">
+        <h2>Login</h2>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
 
-//           <button className="login-btn">Login</button>
-
-//           <p className="signup-text">
-//             Don’t have an account? <a href="#">Signup</a>
-//           </p>
-//         </div>
-//       </div>
-//       <div className="login-image"></div>
-//     </div>
-//   );
-// };
-
-// export default Login;
+export default Login;
